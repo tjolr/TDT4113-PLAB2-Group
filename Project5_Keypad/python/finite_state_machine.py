830ad13ee38892c8538bdeb8e5494007039f2b79
Tjøl Ravndal <tjol.ravndal@gmail.com>
2019-10-11 16:16:43 +0200

    adding KPC methods from rules

--- Project5_Keypad/python/finite_state_machine.py
+++ Project5_Keypad/python/finite_state_machine.py
@@ -1,5 +1,7 @@
 '''Finite state machine'''
 from inspect import isfunction
+from kpc import *
+
 
 class FSM:
     '''Finite state machine class'''
@@ -15,7 +17,7 @@ class FSM:
         '''Add a rule to the end of the rule_list'''
         self.rule_list.append(rule)
 
-    def get_next_signal(self, debug = True):
+    def get_next_signal(self, debug=True):
         '''query the agent for next signal'''
 
         self.symbol = self.agent.get_next_signal()
@@ -30,7 +32,6 @@ class FSM:
                 self.fire_rule(rule)
                 break
 
-
     def apply_rule(self, rule):
         '''Applying a rule, checking whether the conditions are met'''
         return rule.match(self.state, self.symbol)
@@ -43,50 +44,81 @@ class FSM:
         self.agent.do_action(rule.action, self.symbol)
 
         if debug:
-            print("FSM is now in state: ",self.state)
-
+            print("FSM is now in state: ", self.state)
 
     def main(self):
         '''Main method'''
 
-        #Setting up the FSM with the correct rules
+        # Setting up the FSM with the correct rules
         # Rules that are representing one arc each in the full FSM graph.
         # Setting the rules in the order which makes specific-before-general
 
         # From state s_init
-        R1 = Rule("s_init", "s_read", signal_accept_all, "reset_password_accumulator")
+        rule_1 = Rule(
+            "s_init",
+            "s_read",
+            signal_accept_all,
+            KPC.reset_password_accumulator)
 
         # From state s_read
-        R3 = Rule("s_read", "s_verify", "*", "verify_login")
-        R2 = Rule("s_read", "s_read", signal_is_digit, "append_next_password_digit")
-        R13 = Rule("s_read", "s_init", signal_accept_all, "reset_agent")
+        rule_3 = Rule("s_read", "s_verify", "*", KPC.verify_login)
+        rule_2 = Rule(
+            "s_read",
+            "s_read",
+            signal_is_digit,
+            KPC.append_next_password_digit)
+        rule_13 = Rule("s_read", "s_init", signal_accept_all, KPC.reset_agent)
 
         # From state s_verify
-        R5 = Rule("s_verify", "s_active", "Y", "fully_activate_agent")  # Not sure about agent action here
-        R4 = Rule("s_verify", "s_init", signal_accept_all, "reset_agent")
+        # Not sure about agent action here
+        rule_5 = Rule("s_verify", "s_active", "Y", KPC.fully_activate_agent)
+        rule_4 = Rule("s_verify", "s_init", signal_accept_all, KPC.reset_agent)
 
         # From state s_active
-        R6 = Rule("s_active", "s_read_2", "*", "init_passcode_entry")
+        rule_6 = Rule("s_active", "s_read_2", "*", KPC.init_passcode_entry)
 
         # From state s_read_2
-        R8 = Rule("s_read_2", "s_read_3", "*", "cache_1st_new_password")
-        R7 = Rule("s_read_2", "s_read_2", signal_is_digit, "get_next_signal")
-        R10 = Rule("s_read_2", "s_active", signal_accept_all, "fully_ activate_agent")  # Not sure about agent action
+        rule_8 = Rule("s_read_2", "s_read_3", "*", KPC.cache_1st_new_password)
+        rule_7 = Rule(
+            "s_read_2",
+            "s_read_2",
+            signal_is_digit,
+            KPC.get_next_signal)
+        rule_10 = Rule("s_read_2", "s_active", signal_accept_all,
+                       KPC.fully_activate_agent)
 
         # From state s_read_3
-        R12 = Rule("s_read_3", "s_active", "*", "fully_ activate_agent")  # Not sure about agent action
-        R9 = Rule("s_read_3", "s_read_3", signal_is_digit, "get_next_signal")
-        R11 = Rule("s_read_3", "s_read_3", signal_accept_all, "fully_ activate_agent")  # Not sure about agent action
-
+        # Not sure about agent action
+        rule_12 = Rule("s_read_3", "s_active", "*", KPC.fully_activate_agent)
+        rule_9 = Rule(
+            "s_read_3",
+            "s_read_3",
+            signal_is_digit,
+            KPC.get_next_signal)
+        rule_11 = Rule("s_read_3", "s_read_3", signal_accept_all,
+                       KPC.fully_activate_agent)
 
         # Same order as the rules above
-        rule_order = [R1, R3, R2, R13, R5, R4, R6, R8, R7, R10, R12, R9, R11]
-        #Adding each rule to the FSM
+        rule_order = [
+            rule_1,
+            rule_3,
+            rule_2,
+            rule_13,
+            rule_5,
+            rule_4,
+            rule_6,
+            rule_8,
+            rule_7,
+            rule_10,
+            rule_12,
+            rule_9,
+            rule_11]
+        # Adding each rule to the FSM
         for rule in rule_order:
             self.add_rule(rule)
 
-
-        #Query for next signal and run rules until the FSM enters its default final state
+        # Query for next signal and run rules until the FSM enters its default
+        # final state
         while self.symbol != "#":
             self.get_next_signal()
             self.run_rules()
@@ -105,42 +137,59 @@ class Rule:
     def match(self, state, symbol):
         '''Checking whether the conditions are met for a rule'''
         if isfunction(self.signal):
-            #Symbol is a function that accepts one argument
+            # Symbol is a function that accepts one argument
             return state == self.state1 and self.signal(symbol)
-        else:
-            return state == self.state1 and symbol == self.signal
+        return state == self.state1 and symbol == self.signal
 
-def signal_is_digit(signal): return 48 <= ord(signal) <= 57
 
-def signal_accept_all(signal): return True
+def signal_is_digit(signal):
+    '''Returns if the signal is a digit'''
+    return 48 <= ord(signal) <= 57
+
+
+def signal_accept_all(signal):
+    '''Accept every signal'''
+    return True
+
 
 class Agent:
     '''Agent class just for testing'''
+
     def __init__(self):
         ''''''
-        self.signals = ["2", "8", "3", "6", "*", "Y", "*","5","1","7", "*", "5", "1", "7", "*"]
+        self.signals = [
+            "2",
+            "8",
+            "3",
+            "6",
+            "*",
+            "Y",
+            "*",
+            "5",
+            "1",
+            "7",
+            "*",
+            "5",
+            "1",
+            "7",
+            "*"]
 
     def get_next_signal(self):
+        '''Get next signal'''
         if self.signals:
             return self.signals.pop(0)
         return "#"
 
     def do_action(self, action, symbol):
-        pass
+        '''Test method'''
+
 
 def main():
     '''Main method for testing'''
     agent = Agent()
     fsm = FSM(agent)
 
-
     fsm.main()
 
 
 main()
-
-
-
-
-
-
